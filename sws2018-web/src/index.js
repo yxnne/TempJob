c338fad6c9dabc86be325a18a2b25da397e68445
iel-mac <506524339@qq.com>
2018-05-29 17:52:04 +0800

    install basic libs ,test antd style

--- sws2018-web/src/index.js
+++ sws2018-web/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// test antd
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+ReactDOM.render(<Button type="primary" >Test Antd is ok??</Button>, document.getElementById('root'));
 registerServiceWorker();
