f7c0a7f6bc624a30994f6ddddc62e1046fdbf09d
iel-mac <506524339@qq.com>
2018-05-30 10:31:32 +0800

    add redux, router and test them

--- sws2018-web/src/index.js
+++ sws2018-web/src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-// test antd
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+// antd's style import test
+// import 'antd/dist/antd.css'; this is useless config it in the package json
 import { Button } from 'antd';
-import 'antd/dist/antd.css';
+// import reducers of application
+import reducers from './reducers';
+
+// import pages in container folder
+import PageLogin from './container/login/login';
+
+// redux调试工具
+const reduxDevTool = window.__REDUX_DEVTOOLS_EXTENSION__?window.__REDUX_DEVTOOLS_EXTENSION__():f=>f;
+const store = createStore(reducers, compose(applyMiddleware(thunk), reduxDevTool));
 
 
+function C1(){
+  return <Button type='primary'>SHIT</Button>;
+};
+function C2(){
+  return <Button type='ghost'>SHIT</Button>;
+}
 
 
-ReactDOM.render(<Button type="primary" >Test Antd is ok??</Button>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+  <Provider stroe={store}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/login" component={PageLogin}></Route>
+          <Route path="/b2" component={C2}></Route>
+        </Switch>
+
+      </div>
+    </BrowserRouter>
+
+  </Provider>
+), document.getElementById('root'));
---
+++ sws2018-web/src/reducers.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// reducers import
+// import { user } from './redux/user.redux';
+// import { notes } from './redux/notes.redux';
+// import { plans } from './redux/plans.redux';
+
+// combine the reducers
+export default combineReducers({ });
