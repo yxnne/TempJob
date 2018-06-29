6e4b99a984672f4ffa754a510618becfd507a705
yxnne <506524339@qq.com>
2018-06-29 18:35:47 +0800

    work not finished

--- sws-simple-web-app/src/index.js
+++ sws-simple-web-app/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// add redux debug tool reduxDevTool
+const reduxDevTool = window.__REDUX_DEVTOOLS_EXTENSION__?window.__REDUX_DEVTOOLS_EXTENSION__():f=>f;
+// make reducers in use, and middleware thunk, reduxDevTool
+const store = createStore(reducers, compose(applyMiddleware(thunk), reduxDevTool));
+ReactDOM.render(
+  (
+    <Provider store={store}>
+
+
+      <BrowserRouter>
+        <div>
+          {/* here Auth */}
+
+          <Switch>
+            {/* here Routes */}
+
+
+          </Switch>
+
+
+        </div>
+      
+      </BrowserRouter>
+
+    </Provider>
+  )
+  
+  
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ sws-simple-web-app/src/reducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+// reducers import
+
+
+// combine the reducers
+export default combineReducers({  });
