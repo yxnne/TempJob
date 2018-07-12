a9173481235e7f052edad90b9dad8ed2da33e118
DESKTOP-1V6UF7O\yxnne <506524339@qq.com>
2018-07-12 22:11:07 +0800

    add detail list onto staff card

--- sws-simple-web-app/src/component/staffDetailCard/StaffDetailCard.js
+++ sws-simple-web-app/src/component/staffDetailCard/StaffDetailCard.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { Card } from 'antd-mobile';
+import { Card, WingBlank, List  } from 'antd-mobile';
 
 import TextAvatar from '../textAvatar/TextAvatar';
 
 // 样式定义
-const cardStyle = {minHeight:420};
+const cardStyle = {minHeight:420, paddingBottom:20};
 const rankInfoContainer = {  width:'100%', height:60 };
 const rankTextStyle = { fontSize:36, margin:'0px 8px'}
 const infoContainer = { textAlign:'center', fontSize:18};
-const infoItemStyle = { margin:'10px auto', fontSize:18 };
+const infoItemStyle = { margin:'10px auto', fontSize:18, paddingTop:4 };
 const mainTimesTextStyle = { fontSize:32, margin:'0px 8px'};
 
 
+
+const makeDetailContent = (position, times) =>{
+  return (
+    <div style={{display:'table', width:'100%'}}>
+      <div style={{display:'table-row',  width:'100%'}}>
+        <div style={{marginLeft:12, display:'table-cell',  width:'60%', textAlign:'center'}}>
+        {position}
+        </div>
+        <div style={{marginLeft:12, display:'table-cell',  width:'40%', textAlign:'center'}}>
+        {times}<span style={{fontSize:12, marginLeft:8}}>次</span>
+        </div>
+      </div>
+    </div>
+
+  );
+};
 
 /** 员工详情的卡片 */
 export default class StaffDetailCard extends Component {
@@ -19,7 +35,7 @@ export default class StaffDetailCard extends Component {
   constructor(props){
     super(props);
     this.state = {
-      detailShow:true
+      detailShow:false
     };
 
     this.toggleDetailShow = this.toggleDetailShow.bind(this);
@@ -35,11 +51,11 @@ export default class StaffDetailCard extends Component {
   render() {
     // 更多细节的按钮
     const moreEle = (
-      <div style={{ marginTop:50 }} onClick={this.toggleDetailShow}>
-        <div>{this.state.detailShow?'收起展开':'更多细节'}</div>
-        <div>
-          <img style={{ width:40,height:40 }} src={this.state.detailShow?require('./image/up.svg'):require('./image/down.svg')} alt=''/>
-        </div>
+      <div style={{ marginTop:50 ,padding:10, color:'#33A3F4'}} onClick={this.toggleDetailShow}>
+
+        {this.state.detailShow ? '收起展开' : '更多细节'}&nbsp;&nbsp;&nbsp;
+        <img style={{ width:18,height:18, marginTop:6 }} src={this.state.detailShow?require('./image/up.svg'):require('./image/down.svg')} alt=''/>
+
       </div>
     );
     return (
@@ -63,13 +79,30 @@ export default class StaffDetailCard extends Component {
                 共计洗手<span style={mainTimesTextStyle}>144</span>次
               </div>
 
+              {/* 更多按钮 */}
               { moreEle }              
 
+              {/* 细节组件 */}
               {
                 this.state.detailShow?
                 (
-                  <div>这里细节出现</div>
-                ):(<div></div>)
+                  <WingBlank>
+                    <List >
+                      <List.Item>
+                        { makeDetailContent('A处液瓶', 40)}
+                      </List.Item>
+                      <List.Item>
+                        { makeDetailContent('B处液瓶', 60)}
+                      </List.Item>
+                      <List.Item>
+                        { makeDetailContent('C处液瓶', 140)}
+                      </List.Item>
+                      <List.Item>
+                        { makeDetailContent('D处液瓶', 20)}
+                      </List.Item>
+                    </List>
+                  </WingBlank>
+                ):(null)
               }
 
             </div>
