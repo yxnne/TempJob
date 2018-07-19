c2024f9390baa63fd36faac517c80230aa5688be
yxnne <506524339@qq.com>
2018-07-19 13:45:46 +0800

    添加dateutil.js处理date格式

--- sws-simple-web-app/src/util/dateUtils.js
+++ sws-simple-web-app/src/util/dateUtils.js
@@ -41,21 +41,23 @@ function _formatDateTime(theDate) {
   var _year = theDate.getFullYear()
   var _month = theDate.getMonth();
   var _date = theDate.getDate();
-  if(_hour<10){
-    _hour="0"+_hour
+
+  if(_hour < 10){
+    _hour = "0" + _hour
   }
-  if(_minute<10){
-    _minute="0"+_minute  
+  if(_minute < 10){
+    _minute = "0" + _minute  
   }
-  if(_second<10){
-    _second="0"+_second  
+  if(_second < 10){
+    _second = "0" + _second  
   }
+  
   _month = _month + 1;
   if(_month < 10){
     _month = "0" + _month;
   }
-  if(_date<10){
-    _date="0"+_date  
+  if(_date < 10){
+    _date = "0" + _date  
   }
   return  _year + "-" + _month + "-" + _date + " " + _hour + ":" + _minute + ":" + _second ;
 }
