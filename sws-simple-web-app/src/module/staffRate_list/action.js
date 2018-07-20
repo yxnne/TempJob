164bcb9cd364bb795c5a88329ab52df280234ac4
yxnne <506524339@qq.com>
2018-07-20 18:39:49 +0800

    staff rate list not finish

--- sws-simple-web-app/src/module/staffRate_list/action.js
+++ sws-simple-web-app/src/module/staffRate_list/action.js
@@ -1,5 +1,29 @@
 import * as actionType from './actionType';
+import axios from 'axios';
+
+// url
+const URL_STAFFLIST_GET = '/iel-hhms/web/app/getStaffCounts.action';
 
 export const testAction = ()=>({
   type: actionType.TEST_DEPARTMENT_LIST_STATISTIC
 });
+
+export const getAction = ( result ) => ({
+  type: actionType.GET_DEPARTMENT_LIST_STATISTIC, payload:result
+});
+
+// 对外暴露方法来Get到数据
+export const getStaffList = ( startTime, endTime ) => {
+
+  return dispatch =>{
+    axios.get(URL_STAFFLIST_GET, {
+      params:{ startTime, endTime }
+    }).then(res =>{
+      if(res.code === 200 ) {
+        console.log(res);
+      }
+      // dispatch(getAction());
+
+    })
+  }
+};
--- sws-simple-web-app/src/page/AdminMainHospitalPage.js
+++ sws-simple-web-app/src/page/AdminMainHospitalPage.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
-import { WingBlank, WhiteSpace } from 'antd-mobile'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { WingBlank, WhiteSpace } from 'antd-mobile';
 import { view as OrganizationStatistic } from '../module/organization_statistic';
-import { view as DepartmentListStatisttic} from '../module/department_list'
+import { view as DepartmentListStatisttic} from '../module/department_list';
+import { getOrganizationInfo } from '../module/organization_statistic';
+
+const HOSPITAL_ID = 1;
 /**
  * Admin系统主页
  */
+@connect(
+  null,
+  dispatch =>({
+    getOrganizationInfo:(departmentId)=>dispatch(getOrganizationInfo(departmentId))
+  })
+)
 export default class AdminMainHospitalPage extends Component {
+
+  componentDidMount(){
+    this.props.getOrganizationInfo(HOSPITAL_ID);
+  }
+
   render() {
     return (
       <div>
