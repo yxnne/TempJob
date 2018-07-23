760259b3e16f16a39f3cbc239cc0e2d5b6e43744
yxnne <506524339@qq.com>
2018-07-23 23:06:04 +0800

    add rules to staff detail data parse

--- sws-simple-web-app/src/module/staffRate_list/reducer.js
+++ sws-simple-web-app/src/module/staffRate_list/reducer.js
@@ -2,23 +2,26 @@ import * as actionType from './actionType'
 
 const initState = {
   list:[
-    { name:'谢贤', times:47, rank:1, role:'护士', department:'消化科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'马化腾', times:30, rank:2, role:'护士', department:'牙科', detail:[{position:'A处', times:30}] },
-    { name:'王祖蓝', times:28, rank:3, role:'医生' , department:'泌尿科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'关谷神奇', times:27, rank:4, role:'医生', department:'牙科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'郭芙蓉', times:27, rank:5, role:'护士', department:'泌尿科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'佘太君', times:27, rank:6, role:'护师', department:'泌尿科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'邓九公', times:26, rank:7, role:'医生', department:'泌尿科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'土行孙', times:21, rank:8, role:'医生', department:'泌尿科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'武松', times:20, rank:9, role:'护士', department:'妇科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
-    { name:'宝钗', times:19, rank:10, role:'护士', department:'牙科', detail:[{position:'A处', times:30}, {position:'B处', times:17}] },
+    { name:'加载中', times:100, rank:1, role:'加载中', department:'加载中', detail:[{position:'加载中', times:30, name:'1234'}, {position:'加载中', times:17, name:'1234'}] },
+    { name:'加载中', times:100, rank:2, role:'加载中', department:'加载中', detail:[{position:'加载中', times:30, name:'1234'}] },
+    { name:'加载中', times:100, rank:3, role:'加载中' , department:'加载中', detail:[{position:'加载中', times:30, name:'1234'}, {position:'加载中', times:17,name:'1234'}] },
+
   ]
 };
 
 export default (state=initState, action) =>{
   switch(action.type){
     case actionType.GET_STAFF_LIST_STATISTIC:
-      let newList = action.payload;
+      
+      // 需要解析下数据
+      let newList = action.payload.map(item=>{
+        const oldDetail = item.detail;
+        // let posAndName = item.detail.position.split('#');
+        const detail = oldDetail.map(d => (
+          {...d, position:d.position.split('#')[0], name:d.position.split('#')[1] }
+        ));
+        return {...item , detail}
+      });
       return {...state, list:newList };
     default:
       return { ...state }
